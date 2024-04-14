3b9e2bc8e27c5d7c36c29f24bd60aafc66b49b5d
n1amz9 <ignilickiy791@gmail.com>
2024-04-15 02:33:00 +0300

    lol

--- Base/Stage_1/print_inputs.py
+++ Base/Stage_1/print_inputs.py
@@ -2,19 +2,139 @@
 #language = input()
 #print (language, '- отличный выбор')
 
-
-
+#=================
 #name = input()
 #print ("Привет,", name)
 
+#=================
 #I was
 #born
 #this way
 
-string1 = input()
-string2 = input()
-string3 = input()
+#string1 = input()
+#string2 = input()
+#string3 = input()
 
-print (string1)
-print (string2)
-print (string3)
+#=================
+
+#print (string1)
+#print (string2)
+#print (string3)
+
+#=================
+
+#print('a', 'b', 'c', sep='*') #1
+#print('d', 'e', 'f', sep='**', end='')#2
+#print('g', 'h', 'i', sep='+', end='%')#2
+#print('j', 'k', 'l', sep='-', end='\n') #2
+#print('m', 'n', 'o', sep='/', end='!')#3
+#print('p', 'q', 'r', sep='1', end='%')#3
+#print('s', 't', 'u', sep='&', end='\n')#3
+#print('v', 'w', 'x', sep='%')#4
+#print('y', 'z', sep='/', end='!')#5
+
+#=================
+#separ = input()
+#string1 = input()
+#string2 = input()
+#string3 = input()
+
+#print (string1, string2, string3, sep=separ)
+
+
+#=================
+#s = 0
+#k = 30
+#d = k - 5 #30-5
+#k = 2 * d #25 * 2 = 50
+#s = k - 100 
+#print (s)
+
+#=================
+#x = 3 
+#y = 4 
+#z = x + y #=7
+#z = z + 1 #=8
+##x = y #x=4
+#y = 5
+#x = z + y + 7 #8+5+7
+#print (x)
+
+#=================
+#Cube area
+#V=a3
+#S=6a2.
+
+#a = int(input())
+
+#V = a * a * a
+#S = 6 * a * a
+
+#print ('Объем =', V)
+#print('Площадь полной поверхности =', S)
+
+#=================
+
+#Function f (a,b) = 3(a+b3) + 275b2 - 127a - 41
+
+#a = int(input())
+#b = int(input())
+
+#print ((3*(a+b)*(a+b)*(a+b)) + (275*b*b) - (127 * a) - 41)
+
+
+#=================
+
+#Number prev and after input
+#Следующее за числом 20 число: 21
+#Для числа 20 предыдущее число: 19
+
+#x = int(input())
+#print ("Следующее за числом,", x, "число:", x+1)
+#print ("Для числа", x, "предыдущее число:", x-1)
+
+#=================
+
+#Counter for three computers
+
+#monitor = int(input())
+#sysblock = int(input())
+#keyboard = int(input())
+#mouse = int(input())
+#number_of_PCs = 3
+
+#print ((monitor + sysblock + keyboard + mouse) * number_of_PCs)
+
+ 
+#=================
+
+
+#Simple calculator for two digits
+
+#x = int(input())
+#y = int(input())
+
+#print(x, '+', y, '=', x+y)
+#print(x, '-', y, '=', x-y)
+#print(x, '*', y, '=', x*y)
+
+#=================
+
+#Arithmetic progression
+
+#a1 = int(input())
+#d = int(input())
+#n = int(input())
+
+#progression = a1 + d * (n - 1)
+
+#print (progression)
+
+#=================
+
+#Lol_multiplication 1x---2x---3x---etc
+
+x = int(input())
+
+print (1*x, 2*x, 3*x, 4*x, 5*x, sep='---')
+
