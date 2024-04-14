a219955d888bffc6b470f387b2aedba13b8b0ae0
n1amz9 <ignilickiy791@gmail.com>
2024-04-15 01:34:29 +0300

    test

---
+++ Base/Stage_1/print_inputs.py
@@ -0,0 +1,20 @@
+#print("Какой язык программирования ты изучаешь?")
+#language = input()
+#print (language, '- отличный выбор')
+
+
+
+#name = input()
+#print ("Привет,", name)
+
+#I was
+#born
+#this way
+
+string1 = input()
+string2 = input()
+string3 = input()
+
+print (string1)
+print (string2)
+print (string3)
