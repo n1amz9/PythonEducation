85a809ae50e3b98bfd20dc11263366fb0001d3d0
n1amz9 <56825483+n1amz9@users.noreply.github.com>
2024-04-30 04:07:13 +0300

    stage2

---
+++ Base/Stage_2_Conditional operator/ConditionalOperator.py
@@ -0,0 +1,12 @@
+# i = int(input())
+# if i % 2 != 0:
+#     print(i, 'нечётное')
+# else:
+#     print(i, 'чётное')
+
+#==================================
+
+# When registering on websites, you are required to enter your password twice. This is done for security, as this approach reduces the possibility of incorrect password entry.
+
+# Write a program that compares the password and its confirmation. If they match, the program outputs: "Password accepted", otherwise: "Password not accepted".
+ 
