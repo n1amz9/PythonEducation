59e59cdca9aa12ce274d80d713526feaf7b0302e
n1amz9 <56825483+n1amz9@users.noreply.github.com>
2024-07-30 20:34:12 +0300

    added the xml with currents

--- Projects/Currency_converter/currency_converter.py
+++ Projects/Currency_converter/currency_converter.py
@@ -1,16 +1,22 @@
 import requests
+import xmltodict
 
-#https://www.cbr-xml-daily.ru/daily_utf8.xml
+APIurl = "https://www.cbr-xml-daily.ru/daily_utf8.xml"
 
 def get_current_currents():
-    return None
-
+    try:
+        upd_currents = requests.get(APIurl)
+        upd_currents = xmltodict.parse(upd_currents)
+    except Exception as e:
+        print (f"!ERROR!: {e}")
+        return None
+    
 def converter():
     return None
 
 
 def main() :
-    print ("It's only main")
+    test = get_current_currents()
     
 if __name__ == "main":
     main()
