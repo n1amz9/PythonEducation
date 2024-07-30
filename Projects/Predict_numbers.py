25d6d291e49966e93c5e2dc51fc445d3705fb4d3
n1amz9 <56825483+n1amz9@users.noreply.github.com>
2024-07-30 15:24:12 +0300

    done

--- Projects/Predict_numbers.py
+++ Projects/Predict_numbers.py
@@ -6,25 +6,11 @@ def check_input(x): ## Проверка числа
     inp = int(inp)
     if (inp != x):
         if (inp > x):
-            if ((inp - x) >= 20) :
-                print(f"Ваше число сильно больше загаданного числа (>20). Попробуйте ещё раз!")
-                check_input(x)
-            elif ((inp - x) <= 5) :
-                print(f"Почти угадали. Ваше число чуть больше загаданного числа (>5). Попробуйте ещё раз!")
-                check_input(x)
-            else:
-                print(f"Ваше число немного больше загаданного числа. Попробуйте ещё раз!")
-                check_input(x)
+           print(f"Ваше число немного больше загаданного числа. Попробуйте ещё раз!")
+           check_input(x)
         elif (inp < x):
-            if ((x - inp) >= 20) :
-                print(f"Ваше число сильно меньше загаданного числа (>20). Попробуйте ещё раз!")
-                check_input(x)
-            elif ((x - inp) >= 5) :
-                print(f"Почти угадали. Ваше число меньше загаданного числа (>5). Попробуйте ещё раз!")
-                check_input(x)
-            else:
-                print(f"Ваше число немного меньше загаданного числа(<20). Попробуйте ещё раз!")
-                check_input(x)
+            print(f"Ваше число меньше загаданного числа. Попробуйте ещё раз!")
+            check_input(x)
     else:
         print(f"Угадали! Загаданное компьютером число было {x}")
 
@@ -37,7 +23,6 @@ def start_function(ran_number): ## Начало игры
     max_num = int(max_num)
     print("\n")
     ran_number = random.randint(min_num, max_num)
-    print(ran_number)
     print(f"Загадали число от {min_num} до {max_num}. Попробуйте его угадать)")
     return ran_number
 
