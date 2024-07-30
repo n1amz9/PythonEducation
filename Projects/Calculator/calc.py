67e2891f97f0e4b131ca28c8ccf1508ed45d46f1
n1amz9 <56825483+n1amz9@users.noreply.github.com>
2024-07-30 17:00:20 +0300

    base_calculator_ready

--- Projects/Calculator/calc.py
+++ Projects/Calculator/calc.py
@@ -1,11 +1,46 @@
-def calc():
-    return None
+def calc(num1, num2, sign):
+    try:
+        match sign:
+            case "+":
+                print (f"Результат {num1} {sign} {num2} = {num1 + num2}" )
+            case "-":
+                print (f"Результат {num1} {sign} {num2} = {num1 - num2}" )
+            case "*":
+                print (f"Результат {num1} {sign} {num2} = {num1 * num2}" )
+            case "/":
+                print (f"Результат {num1} {sign} {num2} = {num1 / num2}" )
+            case "%":
+                print (f"Результат {num1} {sign} {num2} = {num1 % num2}" )
+            case "^":
+                print (f"Результат {num1} {sign} {num2} = {num1 ** num2}" )
+            case _:
+                print("Вы ввели неверный знак оператор, или тот, кто пока не поддержан. Попробуйте снова")
+    except ZeroDivisionError:
+        print("Делить на ноль нельзя!")
+        
+def check_nums(x):
+    try:
+        int(x)
+        return True
+    except ValueError:
+        return False
 
 def start():
-    return None
+    print("Приветствуем в самом 'продвинутом' калькуляторе")
+    num1 = input("Введите первое число: ")
+    if (check_nums(num1) == False):
+        print("Вы ввели не числовое значение. Попробуйте снова")
+        return None
+    num2 = input("Введите второе число: ")
+    if (check_nums(num2) == False):
+        print("Вы ввели не числовое значение. Попробуйте снова")
+        return None
+    sign = input("Введите математический знак (поддержаны +-*.%^): ")
+    return(int(num1), int(num2), sign)
 
 def main():
-    print("POPA")
+    num1, num2, sign = start()
+    calc (num1, num2, sign)
     
 if __name__ == "__main__":
     main()
