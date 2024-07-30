1189663c3122bf6ea471e7f81ad92d7afeebcaaa
n1amz9 <56825483+n1amz9@users.noreply.github.com>
2024-07-30 20:21:08 +0300

    begin

---
+++ Projects/Currency_converter/currency_converter.py
@@ -0,0 +1,16 @@
+import requests
+
+#https://www.cbr-xml-daily.ru/daily_utf8.xml
+
+def get_current_currents():
+    return None
+
+def converter():
+    return None
+
+
+def main() :
+    print ("It's only main")
+    
+if __name__ == "main":
+    main()
