a9df1b962abcd2ac7ef856bb460b837bd62540f2
n1amz9 <56825483+n1amz9@users.noreply.github.com>
2024-07-30 23:53:57 +0300

    test

--- Projects/Currency_converter/currency_converter.py
+++ Projects/Currency_converter/currency_converter.py
@@ -1,26 +1,25 @@
 import requests
-import xmltodict
+import json
 
 # https://app.freecurrencyapi.com/dashboard
 
 URL = "https://www.cbr-xml-daily.ru/daily_utf8.xml"
+new_path="D:/CodingForDummies/Python/PythonEducation/Projects/Currency_converter/currency_values.json"
 
-def get_current_currents():
-    try:
-        upd_currents = requests.get(URL)
-        upd_currents = xmltodict.parse(upd_currents)
-        return upd_currents
-    except Exception as e:
-        print (f"!ERROR!: {e}")
-        return None
+def get_currents():
+    with open ("D:/CodingForDummies/Python/PythonEducation/Projects/Currency_converter/currency_values.json", mode='r') as read_json :
+        allCurrencies = json.load(read_json)
+        return(allCurrencies)
+
     
 def converter():
     return None
 
 
 def main() :
-    test = get_current_currents()
+    test = get_currents()
     print (test)
+    print (type(test))
     
 if __name__ == "main":
     main()
