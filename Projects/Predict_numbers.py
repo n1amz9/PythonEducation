b7e2ca8ef9f2c6251208cff8f8f5a8ee12651c4a
n1amz9 <56825483+n1amz9@users.noreply.github.com>
2024-07-30 14:30:45 +0300

    starting

--- Projects/Predict_numbers.py
+++ Projects/Predict_numbers.py
@@ -1,2 +1,21 @@
-number = 0
+import random
 
+
+def check_input(x):
+    return None
+
+def main():
+    number = random.randint(0, 50)
+    input = -100
+    
+    print("Вас приветствует игра в угадайку :)")
+    print("\n")
+    min_num= input("Введите минимальное число для генерации: ")
+    max_num= input("Введите максимальное число для генерации: ")
+    
+    print (min_num)
+    print (max_num)
+    print(number)
+    
+if __name__ == "__main__":
+    main()
