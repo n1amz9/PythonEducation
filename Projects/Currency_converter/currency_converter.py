d5642babdb6b4bec6473b71446d728b0ddea4329
n1amz9 <56825483+n1amz9@users.noreply.github.com>
2024-07-30 20:49:34 +0300

    start

--- Projects/Currency_converter/currency_converter.py
+++ Projects/Currency_converter/currency_converter.py
@@ -1,12 +1,13 @@
 import requests
 import xmltodict
 
-APIurl = "https://www.cbr-xml-daily.ru/daily_utf8.xml"
+URL = "https://www.cbr-xml-daily.ru/daily_utf8.xml"
 
 def get_current_currents():
     try:
-        upd_currents = requests.get(APIurl)
+        upd_currents = requests.get(URL)
         upd_currents = xmltodict.parse(upd_currents)
+        return upd_currents
     except Exception as e:
         print (f"!ERROR!: {e}")
         return None
@@ -17,6 +18,7 @@ def converter():
 
 def main() :
     test = get_current_currents()
+    print (test)
     
 if __name__ == "main":
     main()
