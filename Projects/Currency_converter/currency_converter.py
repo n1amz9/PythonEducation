a50cf375c290a482d0a08091d10ab01c8d44c5e3
n1amz9 <56825483+n1amz9@users.noreply.github.com>
2024-07-31 17:55:44 +0300

    test

--- Projects/Currency_converter/currency_converter.py
+++ Projects/Currency_converter/currency_converter.py
@@ -13,21 +13,43 @@ def get_currents():
         allCurrencies = json.load(read_file)
         return(allCurrencies)
   
-def converter():
-    return "TEST"
-
+def converter(num1, cur1, cur2):
+    outputCurrents = get_currents()
+    
+    for outputCurrent in outputCurrents :
+        isFound = False
+        if outputCurrent:
+            for getOutput in outputCurrent["ValCurs"]["Valute"]:
+                if getOutput.get["CharCode"] == cur1:
+                    print (getOutput.get["Value"])
+                    result = float(getOutput.get["Value"]) / num1
+                    isFound = True
+        return (result, cur1, cur2 , isFound)
+        
+def check_converter(result, isFound):
+    try:
+        if (isFound == False):
+            raise ValueError("Вашей валюты нет в списке!")
+    except Exception as e:
+        print(f"Произошла ошибка!: {e}")
+        
 def start():
     print ("Welcome to the Currency converter owo")
     print ("==================================")
     num1 = input("Введите количество валюты: ")
     cur_or = input ("Введите название валюты: (к примеру RUB, BYN): ")
-    cur_dest = input ("Введите валюту, в которую нужно перевести: ")
+    cur_dest = input ("Введите валюту, в которую нужно перевести: (USD, EUR, etc) ")
     
-    return (int(num1), cur_or, cur_dest)
+    return (int(num1), cur_or, cur_dest)  # возвращаем кол-во валюты, конвертируемую валюту, и конечную валюту
 
 def main() :
-    start()
-    
+    # num1, cur_or, cur_dest = start()
+    # converter(num1, cur_or, cur_dest)
+    outputCurrents = get_currents()
+    a = "RUB"
+    print(outputCurrents["ValCurs"]["Valute"])
+    for outputCurrent in outputCurrents:
+        print(outputCurrent[1])
     
 if __name__ == "__main__":
     main()
